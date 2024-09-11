82e8bdf7949adc8d4e3fc32175b34fcb96b11e8a
Rifai Kacanegara <rifaikacanegoro@gmail.com>
2024-09-12 04:22:28 +0700

    fix ui - semoga final commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,6 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
+    <div class="py-12 bg-[#e9f3f2]">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-
             <!-- Embed Tableau Visualization -->
             <div class="p-6 text-gray-900 dark:text-gray-100">
                 <div class='tableauPlaceholder' id='viz1726076207150' style='position: relative'>
